adeb9f3e126faa077178372602b9ee7d9f4ed2ac
matthieulpro <matthieu.lu.pro@gmail.com>
2021-07-28 13:39:49 +0200

    Add link to root in component HeaderWithImage

--- components/molecules/headerWithImage/index.tsx
+++ components/molecules/headerWithImage/index.tsx
@@ -2,6 +2,7 @@ import ImageCircle from '../../atoms/imageCircle'
 import {ImageSize} from '../../atoms/imageCircle'
 import typographyStyles from '../../../styles/typographies.module.css'
 import styles from './header.module.css'
+import Link from 'next/link'
 
 interface HeaderWithImageProps {
   source: string,
@@ -12,8 +13,12 @@ interface HeaderWithImageProps {
 export default function HeaderWithImage({source, name, size}: HeaderWithImageProps) {
   return (
     <header className={styles.header}>
-      <ImageCircle src={source} name={name} size={size} />
-      <h1 className={typographyStyles.heading2Xl}>{name}</h1>
+      <Link href="/">
+        <a><ImageCircle src={source} name={name} size={size} /></a>
+      </Link>
+      <h1 className={typographyStyles.heading2Xl}>
+        {name}
+      </h1>
     </header>
   )
 }
