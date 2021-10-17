351b85e4c0e7344ea79cb2142b117636431b8745
matthieulpro <matthieu.lu.pro@gmail.com>
2021-10-17 07:29:26 +0200

    Test

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const NAME = 'Malus';
 export const WEBSITE_TITLE = "Dev from R'lyeh";
 export const TITLE = "Malus' blog";
 export const DESCRIPTION =
-  "Blog personnel de Malus. Developpeur informatique, je publie des articles tech et gaming qui me permettent d'approfondir mes connaissances sur les sujets.";
+  'Articles about game design, development and personnal stuffs :)';
 export const WEBSITE_URL = 'https://malus.dev/';
